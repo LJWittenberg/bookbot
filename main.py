8af9a6da7a6ac39e5c362ad52bc45ac3658fb2fe
LJWittenberg <lennwitt@freenet.de>
2024-03-12 18:28:55 +0100

    bookbot progress: the bookbot is capable to now count the words in the given book: split up code in seperate functions (not all)

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+def count_words(text):
+    counter = 0
+    words = text.split()
+    for word in words:
+        counter += 1
+    return counter
+#
+def print_book(contents):
+    print(f"{contents}")
+
+#
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(f"{file_contents}")
+    print(f"{print_book(file_contents)} This book has {count_words(file_contents)} words.")
 main()
