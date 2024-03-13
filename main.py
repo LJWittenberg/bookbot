961e452cfdb3e8cda0e598206154d9ba9ac19b2e
LJWittenberg <lennwitt@freenet.de>
2024-03-13 13:18:26 +0100

    bookbot progress: added help function to create a list with lowercase letters.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+def get_lower_case_alpha_list():
+    # initialise an empty list
+    list = []
+    # filling the list with lowercase letter in alphabetical order
+    alpha = 'a'
+    for i in range(0, 26):
+        list.append(alpha)
+        alpha = chr(ord(alpha) + 1)
+    return list
+#
 def count_words(text):
     counter = 0
     words = text.split()
@@ -12,9 +22,15 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 #
+#def count_letters(text):
+ #   lower_text = text.lower()
+  #  letters = 
+   # counted_letters = {}
+
+#
 def main():
     book_path = "books/frankenstein.txt"
     file_contents = get_book_text(book_path)
     print_book(file_contents)
-    print(f"This book has {count_words(file_contents)} words.")
+    print(f"This book has {count_words(file_contents)} words. \n {len(file_contents)-1}")
 main()
